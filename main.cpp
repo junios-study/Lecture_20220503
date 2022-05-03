b82ee79bd76b4b051dfa75185979892cd7f884f0
junios <junios@naver.com>
2022-05-03 15:16:15 +0900

    카드 섞기

--- main.cpp
+++ main.cpp
@@ -4,16 +4,25 @@ using namespace std;
 
 #define MAX 52
 
+//Heart A           0 - 12    0
+//Spade A           13 - 25   1
+//Clover A          26 - 38   2
+//Diamond A         39 - 51   3
+
+
 
 int main()
 {
     int A[MAX] = { 0, };
+    int SplitCard[52];
+
 
     srand((unsigned int)time(0));
 
+    //Initialize
     for (int i = 0; i < MAX; ++i)
     {
-        A[i] = i + 1;
+        A[i] = i;
     }
 
     //shuffle
@@ -28,10 +37,53 @@ int main()
         A[SecondNumber] = Temp;
     }
 
-    for (int i = 0; i < MAX; ++i)
+    //split
+    for (int i = 0; i < 2; ++i)
     {
-        cout << A[i] << ", ";
+        SplitCard[i] = A[i];
     }
+
+    for (int i = 0; i < 2; ++i)
+    {
+        int Type = SplitCard[i] / 13;
+        int Number = SplitCard[i] % 13;
+
+        switch (Type)
+        {
+        case 0:
+            cout << "¢¾";
+            break;
+        case 1:
+            cout << "¢¼";
+            break;
+        case 2:
+            cout << "¢À";
+            break;
+        case 3:
+            cout << "¡ß";
+            break;
+        }
+
+        switch (Number)
+        {
+        case 12:
+            cout << "K" << " ";
+            break;
+        case 11:
+            cout << "Q" << " ";
+            break;
+        case 10:
+            cout << "J" << " ";
+            break;
+        case 0:
+            cout << "A" << " ";
+            break;
+        default:
+            cout << Number + 1 << " ";
+            break;
+        }
+    }
+
 
 
 
